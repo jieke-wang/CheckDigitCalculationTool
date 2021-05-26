5ee661e418d8d5631f409a4a9f1eaebc4c9a8cc8
yihu wang <wangyihu2009@msn.com>
2021-05-26 15:24:06 +0800

    #

--- CheckDigitCalculationTool/MainWindow.xaml.cs
+++ CheckDigitCalculationTool/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,7 +35,7 @@ namespace CheckDigitCalculationTool
         {
             try
             {
-                var data = model.ChecksumRes.ConvertStringToHexArray();
+                var data = txtChecksumResFormat.Text.ConvertStringToHexArray();
                 var cs = Checksum.GetChecksum(data);
 
                 model.ChecksumDes = $"{data.ConvertByteArrayToHexString()} {cs:X2}";
@@ -49,7 +50,7 @@ namespace CheckDigitCalculationTool
         {
             try
             {
-                var data = model.ModbusRes.ConvertStringToHexArray();
+                var data = txtModbusResFormat.Text.ConvertStringToHexArray();
                 var cs = Modbus.CalculateCRC16(data);
 
                 var csBytes = BitConverter.GetBytes(cs);
@@ -62,5 +63,74 @@ namespace CheckDigitCalculationTool
                 MessageBox.Show("请检查你的输入");
             }
         }
+
+        private void txtChecksumRes_KeyDown(object sender, KeyEventArgs e)
+        {
+            HandleKeyDown(e);
+        }
+
+        private void txtModbusRes_KeyDown(object sender, KeyEventArgs e)
+        {
+            HandleKeyDown(e);
+        }
+
+        private void HandleKeyDown(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D0:
+                case Key.NumPad0:
+                case Key.D1:
+                case Key.NumPad1:
+                case Key.D2:
+                case Key.NumPad2:
+                case Key.D3:
+                case Key.NumPad3:
+                case Key.D4:
+                case Key.NumPad4:
+                case Key.D5:
+                case Key.NumPad5:
+                case Key.D6:
+                case Key.NumPad6:
+                case Key.D7:
+                case Key.NumPad7:
+                case Key.D8:
+                case Key.NumPad8:
+                case Key.D9:
+                case Key.NumPad9:
+                case Key.A:
+                case Key.B:
+                case Key.C:
+                case Key.D:
+                case Key.E:
+                case Key.F:
+                    break;
+                case Key.Space:
+                default:
+                    e.Handled = true;
+                    return;
+            }
+        }
+
+        private string FormatString(string res)
+        {
+            res = Regex.Replace(res, @"\s", string.Empty).ToUpper();
+            res = Regex.Replace(res, @"(.{2})", "$1 ").Trim();
+            res = Regex.Replace(res, @"([ ]{2,})", " ").Trim();
+            return res;
+        }
+
+        private void txtChecksumRes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtChecksumResFormat.Text = FormatString(model.ChecksumRes);
+        }
+
+        private void txtModbusRes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtModbusResFormat.Text = FormatString(model.ModbusRes);
+        }
     }
 }
+
+// js 校验规则 -- 去空格、加空格
+// https://www.cnblogs.com/yeqrblog/p/10711240.html
